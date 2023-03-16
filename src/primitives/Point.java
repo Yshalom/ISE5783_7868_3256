5bd92a359b557dff4fedcf106be0d165d54a5fb7
yg2sh <yg2shalom@gmail.com>
2023-03-16 17:51:08 +0200

    Commit - 1

---
+++ src/primitives/Point.java
@@ -0,0 +1,34 @@
+package primitives;
+
+import java.util.Vector;
+
+public class Point {
+    protected Double3 xyz;
+    public Point(double x,double y,double z){
+        xyz = new Double3(x,y,z);
+    }
+    public Point (Double3 t){
+        xyz = t;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Point){
+            return xyz.equals((Object)((Point)obj).xyz);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return xyz.toString();
+    }
+    public Point add(Vector v){
+        return (new Point(xyz.add(v.xyz)));
+    }
+    public Vector subtract(Point a)
+    {
+        return (new Vector(xyz.subtract(v.xyz)));
+
+    }
+
+}
