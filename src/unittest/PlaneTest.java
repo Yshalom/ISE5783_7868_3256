941fe6d7e007d7147d98e09b5fda699d4532fc97
DaniYaakov <dani@yatmal.co.il>
2023-04-16 15:52:19 +0300

    Dani - Commit - 5

--- src/unittest/PlaneTest.java
+++
@@ -1,8 +0,0 @@
-package unittest;
-
-import geometries.*;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
-import static org.junit.jupiter.api.Assertions.*;
-public class PlaneTest {
-}
--- src/unittest/VectorTests.java
+++ src/unittest/VectorTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class VectorTest {
+public class VectorTests {
 
     /**
      * This function checks the creating zero vector by three arguments which are contained zero.
@@ -54,5 +54,37 @@ public class VectorTest {
                 () -> assertTrue(v1.add(v2).equals(new Vector(-1, -2, -3))),
                 () -> assertTrue(v1.subtract(v2).equals(new Vector(3, 6, 9)))
         );
+    }
+
+    @Test
+    @DisplayName("VectorTest - test Cross-Product")
+    void Cross_Product(){
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(-2, -4, -6);
+        Vector v3 = new Vector(0, 3, -2);
+        assertThrows(Exception.class, () -> v1.crossProduct(v2));
+        Vector vr = v1.crossProduct(v3);
+        assertAll(
+                () -> assertTrue(isZero(vr.length() - v1.length() * v3.length())),
+                () -> assertTrue(isZero(vr.dotProduct(v1)) || !isZero(vr.dotProduct(v3)))
+
+        );
+    }
+    @Test
+    @DisplayName("VectorTest - test vector normalization vs vector length and cross-product")
+    void tvnvvlacp(){
+        Vector v = new Vector(1, 2, 3);
+        Vector u = v.normalize();
+        assertTrue(isZero(u.length() - 1));
+        assertThrows(Exception.class,()-> v.crossProduct(u));
+        assertTrue(!(v.dotProduct(u) < 0));
+    }
+
+    @Test
+    @DisplayName("VectorTest - est operations with points and vectors")
+    void eowpav(){
+        Point p1 = new Point(1, 2, 3);
+        assertTrue((p1.add(new Vector(-1, -2, -3)).equals(new Point(0, 0, 0))));
+        assertTrue(new Vector(1, 1, 1).equals(new Point(2, 3, 4).subtract(p1)));
     }
 }
