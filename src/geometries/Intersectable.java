379de316dffa9b8c6b68ebe4c85b8a1505b5d465
yg2sh <yg2shalom@gmail.com>
2023-04-29 22:22:34 +0300

    Yshalom - Commit - 16

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -5,9 +5,8 @@ import java.util.List;
 
 /**
  * The Intersectable class is an abstract class representing the findIntsersections function to all the geometries.
- * the findIntsersections fuction provides
+ *the findIntersections fuction provides intersection points on the geometry interface;
  */
 public abstract class Intersectable {
-    public abstract List<Point> findIntsersections(Ray ray);
-
+    public abstract List<Point> findIntersections(Ray ray);
 }
