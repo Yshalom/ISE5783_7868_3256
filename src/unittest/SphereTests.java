8423b92cedcb51823172c35564a804e7cc06519e
yg2sh <yg2shalom@gmail.com>
2023-04-30 18:16:59 +0300

    Yshalom - Commit - 17

--- src/unittest/SphereTests.java
+++ src/unittest/SphereTests.java
@@ -37,7 +37,7 @@ public class SphereTests {
     {
         // ============ Equivalence Partitions Tests ==============
 
-        // TC01: Two intersections
+        // TC01: Ray starts before and crosses the sphere (2 points)
         Sphere sphere = new Sphere(new Point(-3.4, 3.5, 2), 2.3);
         Ray ray = new Ray(new Point(-2.1, 0.6, 1.2), new Vector(0.5, 9.5, 1.1));
         List<Point> intersections = sphere.findIntersections(ray);
@@ -46,52 +46,60 @@ public class SphereTests {
                 && (intersections.indexOf(new Point(-2.038445427961758, 1.769536868726604, 1.335420058484133)) != -1)
         );
 
-        // TC02: One intersection
+        // TC02: Ray starts inside the sphere (1 point)
         sphere = new Sphere(new Point(-2, -3, 1), 3);
         ray = new Ray(new Point(0.2, -1.6, 2.4), new Vector(-4.5, 2,3));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections.indexOf(new Point(-0.740392608481126, -1.182047729563944, 3.026928405654084)) != -1);
 
-        // TC03: No Intersections - the back of the ray point at the sphere.
+        // TC03: Ray starts after the sphere (0 points)
         sphere = new Sphere(new Point(2, -3, 4), 4);
         ray = new Ray(new Point(-2,2,2), new Vector(-2,4,1));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections == null);
 
-        // TC04: No Intersections - either the ray or the back of the ray don't point at the sphere.
+        // TC04: Ray's line is outside the sphere (0 points)
         sphere = new Sphere(new Point(0, -2, 1), 1);
-        ray = new Ray(new Point(0,0,0), new Vector(1, -1, -1));
+        ray = new Ray(new Point(0,0,1), new Vector(1, -1, -1));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections == null);
 
         // =============== Boundary Values Tests ==================
 
-        // TC05: The ray's point is on the surface of the sphere.
+        // **** Group: Ray's line crosses the sphere (but not the center)
+        // TC05: Ray starts at sphere and goes outside (0 points)
         sphere = new Sphere(new Point(1, 2,2), 1);
         ray = new Ray(new Point(1.111295231062723, 1.038064558856064, 2.249586815785443), new Vector(-0.178146236049984, -1.518727444008292, -0.635382530785116));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections == null);
+        // TC06: Ray starts at sphere and goes inside (1 points)
         ray = new Ray(new Point(1.111295231062723, 1.038064558856064, 2.249586815785443), new Vector(0.26890179243615, 2.352730712224302, -0.249586815785443));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections.indexOf(new Point(1.329031130147028, 2.943124143206193, 2.047490692727668)) != -1);
 
-        //TC06: Either the ray or the ray's back point at the center of the sphere
+        // **** Group: Ray's line goes through the center
+        //TC07: Ray starts at sphere and goes outside (0 points)
         sphere = new Sphere(new Point(2, 1.5, 1.3), 3);
         ray = new Ray(new Point(2.33388569318817, -1.385806323431808, 2.048760447356328), new Vector(0.452033416234913, -3.906968515233804, 1.013714423424881));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections == null);
+        // TC08:  Ray starts at the center (1 points)
         ray = new Ray(new Point(2,1.5,1.3), new Vector(0.785919109423083, -6.792774838665611, 1.762474870781208));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections.indexOf(new Point(2.33388569318817, -1.385806323431808, 2.048760447356328)) != -1);
+        // TC09: Ray starts after sphere (0 points)
         ray = new Ray(new Point(2.785919109423083, -5.292774838665611, 3.062474870781208), new Vector(0.785919109423083, -6.792774838665611, 1.762474870781208));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections == null);
+        // TC10: Ray starts at sphere and goes inside (1 points)
         ray = new Ray(new Point(2.33388569318817, -1.385806323431808, 2.048760447356328), new Vector(-1.015720057923762, 8.778966651742628, -2.277818488410566));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections.indexOf(new Point(1.66611430681183, 4.385806323431808, 0.551239552643672)) != -1);
+        // TC11: Ray starts inside (1 points)
         ray = new Ray(new Point(2, 1.5, 1.3), new Vector(0.514930059289592, -0.164680796154549, 0.7));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections.indexOf(new Point(3.746587725597012, 0.941420340459638, 3.674325184287449)) != -1);
+        // TC12: Ray starts before the sphere (2 points)
         ray = new Ray(new Point(-1.46717058096745, 2.608842650330184, -3.41329914207296), new Vector(0.514930059289592, -0.164680796154549, 0.7));
         intersections = sphere.findIntersections(ray);
         assertTrue(
@@ -99,20 +107,24 @@ public class SphereTests {
                         && (intersections.indexOf(new Point(0.253412274402988, 2.058579659540362, -1.074325184287449)) != -1)
         );
 
-        // TC07: Either the ray's direction vector or the back of the ray's direction vector is tangent with the surface of the sphere.
+        // **** Group: Ray's line is tangent to the sphere (all tests 0 points)
+        // TC13:  Ray starts before the tangent point
         sphere = new Sphere(new Point(4.346349994341999, -4.008099017677047, 1.660117325062423), 3.5);
         ray = new Ray(new Point(8.56729184135195, -6.056301512618791, 2.866656582468582), new Vector(-4.687581501582791, 4.669643936487269, 3.52082084249896));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections == null);
+        // TC14:  Ray starts at the tangent point
         ray = new Ray(new Point(6.472604315137252, -3.969629547060173, 4.439966720826513), new Vector(-4.687581501582791, 4.669643936487269, 3.52082084249896));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections == null);
+        // TC15: Ray starts after the tangent point
         ray = new Ray(new Point(5.134308797210932, -2.63645516952613, 5.445154310918958), new Vector(-4.687581501582791, 4.669643936487269, 3.52082084249896));
         intersections = sphere.findIntersections(ray);
         assertTrue(intersections == null);
 
 
-        // TC08: The ray's direction vector is perpendicular to the vector from the sphere's center to the ray's start point.
+        // **** Group: Special cases
+        // TC16: Ray's line is outside, ray is orthogonal to ray start to sphere's center line
         sphere = new Sphere(new Point(3.514965948196176, 3.14311097938581, 0), 2.4);
         ray = new Ray(new Point(0,0,0), new Vector(0,0,1));
         intersections = sphere.findIntersections(ray);
