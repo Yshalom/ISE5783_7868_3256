75f8600b6cb51f12c10dbcbabb9838fc6f879957
DaniYaakov <dani@yatmal.co.il>
2023-05-21 14:56:42 +0300

    Dani - commit - 14

---
+++ src/renderer/RayTracerBase.java
@@ -0,0 +1,14 @@
+package renderer;
+import scene.*;
+import primitives.*;
+
+public abstract class RayTracerBase {
+    protected Scene scene;
+
+    public RayTracerBase(Scene scene)
+    {
+        this.scene = scene;
+    }
+
+    public abstract Color traceRay(Ray ray);
+}
